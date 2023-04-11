1e511562ce2e3cf62cb48bad859e3f6ee50e61ba
ultra1008 <ultra921008@gmail.com>
2023-04-11 09:39:13 -0400

    new

---
+++ account.cmd/account.cmd/src/test/java/com/tamerlan/account/cmd/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamerlan.account.cmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
